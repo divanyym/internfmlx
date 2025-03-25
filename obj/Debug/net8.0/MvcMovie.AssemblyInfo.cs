2e67961c2b76bc10b42be5faae1b36541e725c09
divanyym <divanydyna09@gmail.com>
2025-03-25 10:18:13 +0700

    add conditional compilation (if debug)

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3b17f39b1072f973b0817d7c37cea6cb538cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f339238f8574b5ab3257687ff0013fb4367af690")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
