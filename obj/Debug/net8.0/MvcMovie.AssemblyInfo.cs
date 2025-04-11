adde7c2b0b489f753439cd02d65fd0aa4f9c11d8
divanyym <divany.putri@m3.formulatrix.com>
2025-04-11 10:17:55 +0700

    delete csv data

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ac26895fe31d434614c5db6ce0673766d04e83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d599056e63ec38f4423879a2b2248f867c3e1bad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
