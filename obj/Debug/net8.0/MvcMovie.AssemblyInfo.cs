eafa538c94453e58535f25bd39b9f54aed1780e2
divanyym <divany.putri@m3.formulatrix.com>
2025-04-15 08:11:20 +0700

    Modify login UI

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f653957296afbc4245899d92883097a2136f17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6680cb038cf19bc147c6b3aa32dcf40e32fc8934")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
